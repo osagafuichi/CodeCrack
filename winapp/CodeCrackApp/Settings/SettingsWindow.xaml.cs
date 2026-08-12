a5149060d1d6d3cb55f6f079394df182475fd245
Sam Ethan Mathew <semathew@andrew.cmu.edu>
2026-08-11 23:09:23 -0700

    feat(app): theme picker in Settings (6 editor themes, persists + applies on close)

--- winapp/CodeCrackApp/Settings/SettingsWindow.xaml.cs
+++ winapp/CodeCrackApp/Settings/SettingsWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using CodeCrack.App.Core.Editor;
 using CodeCrack.App.Core.Settings;
 using CodeCrackApp.Services;
 
@@ -16,6 +17,8 @@ public partial class SettingsWindow : Window
         FontSlider.Value = settings.FontSize;
         IndentCombo.SelectedIndex = settings.IndentWidth switch { 2 => 0, 8 => 2, _ => 1 };
         SpacesCheck.IsChecked = settings.IndentUsesSpaces;
+        ThemeCombo.ItemsSource = EditorThemeRegistry.All;
+        ThemeCombo.SelectedValue = settings.EditorTheme;
         EngineBox.Text = settings.EnginePathOverride;
         ApiKeyBox.Password = Dpapi.Unprotect(settings.ClaudeApiKey); // show decrypted (as dots)
     }
@@ -26,6 +29,7 @@ public partial class SettingsWindow : Window
         _settings.IndentWidth = ((ComboBoxItem)IndentCombo.SelectedItem).Content is "2" ? 2
                               : ((ComboBoxItem)IndentCombo.SelectedItem).Content is "8" ? 8 : 4;
         _settings.IndentUsesSpaces = SpacesCheck.IsChecked == true;
+        if (ThemeCombo.SelectedValue is string themeKey) _settings.EditorTheme = themeKey;
         _settings.EnginePathOverride = EngineBox.Text;
         _settings.ClaudeApiKey = Dpapi.Protect(ApiKeyBox.Password); // encrypt before persisting
         _settings.Save();
